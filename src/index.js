f057c90e4b504a6b163621560066884f844be5e9
Filip Filipovic <102156199+RaptorFF@users.noreply.github.com>
2025-12-12 15:07:37 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import reducerAccount from "../src/features/accounts/accountSlice";
-import reducerCustomer from "../src/features/customers/customerSlice";
-import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "@redux-devtools/extension";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
 
-const rootReducer = combineReducers({
-  account: reducerAccount,
-  customer: reducerCustomer,
-});
+import store from "./store";
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
-
-export default store;
