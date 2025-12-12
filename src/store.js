e76a7c129a28d61c5b679f8f682aeefb7ebf0bab
Filip Filipovic <102156199+RaptorFF@users.noreply.github.com>
2025-12-12 15:05:34 +0100

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,12 +1,13 @@
-import { combineReducers, createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+
 import reducerAccount from "../src/features/accounts/accountSlice";
 import reducerCustomer from "../src/features/customers/customerSlice";
 
-const rootReducer = combineReducers({
-  account: reducerAccount,
-  customer: reducerCustomer,
+const store = configureStore({
+  reducer: {
+    account: reducerAccount,
+    customer: reducerCustomer,
+  },
 });
-
-const store = createStore(rootReducer);
 
 export default store;
